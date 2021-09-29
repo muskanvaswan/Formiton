9ef49db1422685b76b8c43a034bfa014d1f71ded
Muskan Vaswan <muskanvaswan@gmail.com>
2021-09-29 23:12:53 +0530

    added validators

---
+++ src/components/forms/Validators.js
@@ -0,0 +1,35 @@
+const validateURL = (value) => {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(value);
+}
+
+const validatePhoneNumber = (val) => {
+  if (val != null)
+    return val.toString().length === 10;
+  else false
+}
+
+const validateEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export default function validator(value, type) {
+  if (type == 'email') {
+    return validateEmail(value)
+  }
+  else if (type == 'number') {
+    return validatePhoneNumber(value);
+  }
+  else if (type == 'url') {
+    return validateURL(value);
+  }
+  else {
+    return true;
+  }
+}
