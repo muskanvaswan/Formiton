71051ce31f5695c6d70a03c9d1b090c04fa2c430
Muskan Vaswan <muskanvaswan@gmail.com>
2021-10-12 23:45:49 +0530

    componentalise more components

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,13 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
-import EditIcon from '@mui/icons-material/Edit';
-import VisibilityIcon from '@mui/icons-material/Visibility';
 import Collapse from '@mui/material/Collapse';
 import Create from '../src/components/index/Create'
-
-import { useRouter } from "next/router";
+import FormsList from '../src/components/index/FormsList'
 
 const classes = {
   root: {
@@ -40,6 +34,7 @@ export default function Index() {
     const res = await fetch(`http://localhost:3000/api/form/search/${emailSearch}`);
     const data = await res.json()
     setForms(data)
+    setCreateOpen(false)
     setFound(true)
   }
 
@@ -49,7 +44,6 @@ export default function Index() {
     }
   }
 
-  const router = useRouter();
 
   return (
     <Box sx={classes.root}>
@@ -68,29 +62,11 @@ export default function Index() {
           </Box>
         }
       </Container>
-      <Collapse in={creatOpen} sx={{width: '100%', height: '40vh', bgcolor: 'rgba(198, 198, 198, 0.22)'}}>
+      <Collapse in={creatOpen} sx={{width: '100%', height: creatOpen? '40vh': '0vh', bgcolor: 'rgba(198, 198, 198, 0.22)'}}>
         <Create />
       </Collapse>
       <Collapse in={found} sx={{width: '100%', minHeight: '40vh', bgcolor: 'rgba(198, 198, 198, 0.22)', }}>
-        <Box sx={{height: '40vh', width: '100%', mt:5, py:2, px: 12,mb: 0, display: 'flex', overflowX: 'scroll'}}>
-          {forms.map((form, idx) => (
-            <Card sx={{ minWidth: 200, m: 2, height: '80%', borderRadius: '10px', position: 'relative'}} key={idx}>
-              <Box sx={{bgcolor: form.theme.bgcolor, height: 100, width: '100%', position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-                <Box sx={{ bgcolor: form.theme.primary, height: '100%', width: 30, borderRadius: '2px', m: 1}} />
-                <Box sx={{ bgcolor: form.theme.secondary, height: '100%', width: 30, borderRadius: '2px', m: 1}} />
-              </Box>
-              <CardContent>
-                <Typography variant="h4" color="text.secondary" gutterBottom>
-                  {form.title}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <IconButton size="small" onClick={() => redirect(`/edit/form/${form.id}`)}><EditIcon sx={{fontSize: 20}}/></IconButton>
-                <IconButton size="small" onClick={() => redirect(`/forms/preview/${form.id}`)}><VisibilityIcon sx={{fontSize: 20}}/></IconButton>
-              </CardActions>
-            </Card>
-          ))}
-        </Box>
+        <FormsList forms={forms} />
       </Collapse>
     </Box>
   );
---
+++ src/components/index/FormsList.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import EditIcon from '@mui/icons-material/Edit';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+export default function FormList({ forms }) {
+  const redirect = (url) => {
+    if (window) {
+      window.open(url, '_blank')
+    }
+  }
+
+  return (
+    <Box sx={{height: '40vh', width: '100%', mt:5, py:2, px: 12,mb: 0, display: 'flex', overflowX: 'scroll'}}>
+      {forms.map((form, idx) => (
+        <Card sx={{ minWidth: 200, m: 2, height: '80%', borderRadius: '10px', position: 'relative'}} key={idx}>
+          <Box sx={{bgcolor: form.theme.bgcolor, height: 100, width: '100%', position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+            <Box sx={{ bgcolor: form.theme.primary, height: '100%', width: 30, borderRadius: '2px', m: 1}} />
+            <Box sx={{ bgcolor: form.theme.secondary, height: '100%', width: 30, borderRadius: '2px', m: 1}} />
+          </Box>
+          <CardContent>
+            <Typography variant="h4" color="text.secondary" gutterBottom>
+              {form.title}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton size="small" onClick={() => redirect(`/edit/form/${form.id}`)}><EditIcon sx={{fontSize: 20}}/></IconButton>
+            <IconButton size="small" onClick={() => redirect(`/forms/preview/${form.id}`)}><VisibilityIcon sx={{fontSize: 20}}/></IconButton>
+          </CardActions>
+        </Card>
+      ))}
+    </Box>
+  )
+}
