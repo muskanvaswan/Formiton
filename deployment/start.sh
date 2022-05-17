9f497c4b34247026bc5311fea54d4b9345e69c55
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-05-17 21:58:12 +0530

    ci: check for env vars while deployment

--- deployment/start.sh
+++ deployment/start.sh
@@ -4,4 +4,15 @@ set -x
 set -o errexit
 set -o nounset
 
+
+if [ -f .env ]; then
+    . ./.env
+fi
+
+if [ -z "${DATABASE_URL}" ]; then
+    echo '"DATABASE_URL" environment variable does not exist'
+    exit 1
+fi
+
+/app/node_modules/.bin/prisma migrate deploy
 /app/node_modules/.bin/next start --port 80
